b92561574c69dac3afcb70f4be56915deef9bb31
ShutaTanaka <shuta.tanaka@eeeeg.tokyo>
2023-09-28 01:57:48 +0900

    リクエスト・リダイレクトなどについて学習

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\UtilityController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\RequestSampleController;
@@ -41,6 +42,13 @@ Route::get('/monty-hall', [GameController::class,'montyHall']);
 //リクエスト
 Route::get('/form', [RequestSampleController::class,'form']);
 Route::get('/query-strings', [RequestSampleController::class,'queryStrings']);
-Route::get('/users/{id}',[RequestSampleController::class,'profile'])->name('profile');
-Route::get('/products/{category}/{year}',[RequestSampleController::class,'productsArchive']);
-Route::get('/route-link',[RequestSampleController::class,'routeLink']);
+Route::get('/users/{id}', [RequestSampleController::class,'profile'])->name('profile');
+Route::get('/products/{category}/{year}', [RequestSampleController::class,'productsArchive']);
+Route::get('/route-link', [RequestSampleController::class,'routeLink']);
+
+//ログイン
+Route::get('/login', [RequestSampleController::class,'loginForm']);
+Route::post('/login', [RequestSampleController::class,'login'])->name('login');
+
+//イベント
+Route::resource('/events',EventController::class)->only(['create','store']);
